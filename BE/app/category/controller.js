219682ad3c4fb5cabfb37f8295aa5a9fbbb2af62
RickyS <suhanryricky@yahoo.com>
2023-03-13 22:14:11 +0700

    Feat: create admin page

--- BE/app/category/controller.js
+++ BE/app/category/controller.js
@@ -1,9 +1,30 @@
+const Category = require('./model');
+
 module.exports = {
     index: async(req, res) => {
         try {
-            res.render('admin/category/view_category')
+            res.render('admin/category/view_category');
         } catch (err) {
-            console.log(err)
+            console.log(err);
+        }
+    },
+    viewCreate : async(req, res) => {
+        try {
+            res.render('admin;/category/category');
+        } catch (err) {
+            console.log(err);
+        }
+    },
+    actionCreate : async(req, res) => {
+        try {
+            const { name } = req.body
+
+            const category = await Category({ name });
+            await category.save();
+
+            res.redirect('/category');
+        } catch (err) {
+            console.log(err);
         }
     }
 }
--- BE/app/category/routers.js
+++ BE/app/category/routers.js
@@ -1,48 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const { index, viewCreate } = require('./controller');
 
-router.get('/', function (req, res) {
-    res.send('Hello World!');
-})
+// GET home page
+router.get('/', index);
+router.get('/create', viewCreate);
+router.post('/create', actionCreate);
 
-router.get('/users/:id', function (req, res) {
-    const id = req.params.id;
-    if (Number(id) === 1) {
-        const user = {
-            id: 1,
-            name: 'ricky suhanry',
-            age: 25
-        }
-        res.send(user);
-    } else {
-        const user = {
-            id: 2,
-            name: 'deswon entinji',
-            age: 23
-        }
-        res.send(user);
-    }
-})
-
-router.get('/users/', function (req, res) {
-    const name = req.query.name;
-    const age = req.query.aga;
-    res.send(name + " " + age);
-    
-})
-
-
-router.post('/users', function (req, res) {
-    console.log("post /user");
-    res.send('Got a POST request');
-})
-
-router.put('/user',  function (req, res) {
-    res.send('Got a PUT request at /user');
-})
-
-router.delete('/user',  function (req, res) {
-    res.send('Got a DELETE request at /user');
-})
-
-module.exports = router
+module.exports = router;
