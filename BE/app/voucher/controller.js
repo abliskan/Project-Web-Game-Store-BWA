1bd07744416196c147b0e88a9f9c9aff70ab4ed0
RickyS <suhanryricky@yahoo.com>
2023-03-23 21:56:22 +0700

    Update: create voucher page

--- BE/app/voucher/controller.js
+++ BE/app/voucher/controller.js
@@ -1,6 +1,9 @@
 const Voucher = require('./model');
 const Category = require('../category/model');
 const Nominal = require('../nominal/model');
+const path = require('path');
+const fs = require('fs');
+const config = require('../../config');
 
 module.exports = {
     index: async(req, res) => {
@@ -35,25 +38,65 @@ module.exports = {
             res.redirect('/voucher');
         }
     },
-/*
+
     actionCreate : async(req, res) => {
         try {
-            const { name } = req.body
+            const { name, category, nominals } = req.body;
 
-            const category = await Category({ name });
-            await category.save();
+            if(req.file) {
+                let tmp_path = req.file.path;
+                let originaExt = req.file.originalname.split('.')[req.file.originalname.split('.').length - 1];
+                let filename = req.file.filename + '.' + originaExt;
+                let target_path = path.resolve(config.rootPath, `public/uploads/${filename}`);
 
-            req.flash('alertMessage', "berhasil tambah kategori");
-            req.flash('alertStatus', "success");
+                const src  = fs.createReadStream(tmp_path);
+                const dest = fs.createWriteStream(target_path);
 
-            res.redirect('/category');
+                src.pipe(dest);
+
+                src.on('end', async ()=> {
+                    try {
+                        const voucher = new Voucher({
+                            name,
+                            category,
+                            nominals,
+                            thumbnail: filename
+                        })
+
+                        await voucher.save();
+
+                        req.flash('alertMessage', "berhasil tambah nominal");
+                        req.flash('alertStatus', "success");
+            
+                        res.redirect('/voucher');
+                    } catch (err) {
+                        req.flash('alertMessage', `${err.message}`);
+                        req.flash('alertStatus', `danger`);
+                        res.redirect('/voucher');
+                    }
+                })
+            } else {
+                const voucher = new Voucher({
+                    name,
+                    category,
+                    nominals,
+                    thumbnail: filename
+                })
+
+                await voucher.save();
+
+                req.flash('alertMessage', "berhasil tambah nominal");
+                req.flash('alertStatus', "success");
+
+                res.redirect('/voucher');
+            }
         } catch (err) {
             req.flash('alertMessage', `${err.message}`);
             req.flash('alertStatus', `danger`);
-            res.redirect('/category');
+            res.redirect('/nominal');
         }
     },
-
+/*
     viewEdit : async(req, res) => {
         try {
             const { id } = req.params;
--- BE/app/voucher/router.js
+++ BE/app/voucher/router.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { index } = require('./controller');
+const { index, viewCreate, actionCreate } = require('./controller');
+const multer = require('multer');
+const os = require('os');
 
 // GET home page
 router.get('/', index);
-/*
 router.get('/create', viewCreate);
-router.post('/create', actionCreate);
+router.post('/create', multer({ dest: os.tmpdir()}).single('image'), actionCreate);
+/*
 router.get('/edit/:id', viewEdit);
 router.put('/edit/:id', actionEdit);
 router.delete('/delete/:id', actionDelete);
