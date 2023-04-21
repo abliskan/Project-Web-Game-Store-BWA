a5112fff31488de30deb3ef1f2a5a0b167bbf467
RickyS <suhanryricky@yahoo.com>
2023-04-21 23:22:45 +0700

    Add: api checkout

--- BE/app/player/controller.js
+++ BE/app/player/controller.js
@@ -1,6 +1,9 @@
 const Player = require('./model');
 const Voucher = require('../voucher/model');
 const Category = require('../category/model');
+const Bank = require('../bank/model');
+const Payment = require('../payment/model');
+const Nominal = require('../nominal/model');
 
 module.exports = { 
     landingPage: async(req, res) => {
@@ -40,5 +43,63 @@ module.exports = {
         } catch (err) {
             res.status(500).json({ message: err.message || `Internal server error` });
         }
+    },
+
+    checkout : async(req, res) => {
+        try {
+            const { accountUser, name, nominal, voucher, payment, bank } = req.body
+            const res_voucher = await Voucher.findOne({ _id : voucher })
+              .populate('name category _id thumbnail user')
+              .populate('category')
+              .populate('user')
+
+            if(!res_voucher) return res.status(404).json({ message: 'voucher game tidak ditemukan.!' });
+
+            const res_nominal = await Nominal.findOne({ _id : nominal })
+            
+            if(!res_nominal) return res.status(404).json({ message: 'nominal tidak ditemukan.!' });
+
+            const res_payment = await Payment.findOne({ _id : payment })
+
+            if(!res_payment) return res.status(404).json({ message: 'payment tidak ditemukan.!' });
+
+            const res_bank = await Bank.findOne({ _id : bank })
+
+            let tax = ( 10 / 100 ) * res_nominal._doc.price;
+            let value = res_nominal._doc.price - tax;
+
+            const payload = {
+                historyVoucherTopup : {
+                    gameName : res_voucher._doc.name,
+                    category: res_voucher._doc.category ? res_voucher._doc.category.name : '',
+                    thumbnail: res_voucher._doc.thumbnail,
+                    coinName: res_voucher._doc.coinName,
+                    coinQuantity: res_voucher._doc.coinQuantity,
+                    price: res_voucher._doc.price,
+                }, 
+                historyPayment : {
+                    name : res_payment._doc.name,
+                    type: res_payment._doc.type,
+                    banName: res_payment._doc.bankName,
+                    noRekening: res_payment._doc.noRekening
+                },
+
+                name : name,
+                accountUser : accountUser,
+                tax : tax,
+                value : value,
+                player : req.player_id,
+                historyUser : {
+                    name : res_voucher._doc.user?._id,
+                    phoneNumber : res_voucher._doc.user?.phoneNumber
+                },
+                category : res_voucher._doc.category?._id,
+                user : res_voucher._doc.user?._id
+            }
+
+            if(!res_bank) return res.status(404).json({ message: 'bank tidak ditemukan.!' });
+        } catch (err) {
+            res.status(500).json({ message: err.message || `Internal server error` });
+        }   
     }
 }
