e1577f485ab75a8c91ff4baa1d6e3d2c40bbe2ba
RickyS <suhanryricky@yahoo.com>
2023-04-27 12:30:27 +0700

    Add: api profile change

--- BE/app/player/router.js
+++ BE/app/player/router.js
@@ -1,16 +1,22 @@
 var express = require('express');
 var router = express.Router();
-const { landingPage, detailPage, category, checkout, history, historyDetail, dashboard } = require('./controller');
+const { landingPage, detailPage, category, checkout, history, historyDetail, dashboard, profile, editProfile } = require('./controller');
 const { isLoginPlayer } = require('../middleware/auth');
+const multer = require('multer');
+const os = require('os');
 
 router.get('/landingpage', landingPage);
 router.get('/:id/detail', detailPage);
 router.get('/category', category);
-router.get('/checkout', checkout);
+router.post('/checkout', checkout);
 router.post('/checkout', isLoginPlayer, checkout);
 router.get('/history', isLoginPlayer, history);
 router.get('/history/:id/detail', isLoginPlayer, historyDetail);
 router.get('/dashboard', isLoginPlayer, dashboard);
 router.get('/profile', isLoginPlayer, profile);
+router.put('/profile', 
+isLoginPlayer, 
+multer({ dest: os.tmpdir() }).single('imnage'),
+editProfile);
 
 module.exports = router;
